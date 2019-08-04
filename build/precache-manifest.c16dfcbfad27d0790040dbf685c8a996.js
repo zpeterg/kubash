fa1785f88c9451b4abf246155dcfe67bef17a104
Peter Granderson <solace@fastmail.fm>
2019-08-04 14:22:54 -0500

    change homepage

---
+++ build/precache-manifest.c16dfcbfad27d0790040dbf685c8a996.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "619555f47fc99f236d67dde13a212599",
+    "url": "/kubash/build/index.html"
+  },
+  {
+    "revision": "e2d7125fcf4b34bb5914",
+    "url": "/kubash/build/static/css/main.466b9ff9.chunk.css"
+  },
+  {
+    "revision": "683a8449ca98b349ab99",
+    "url": "/kubash/build/static/js/2.c9c03324.chunk.js"
+  },
+  {
+    "revision": "e2d7125fcf4b34bb5914",
+    "url": "/kubash/build/static/js/main.e7ea7e8d.chunk.js"
+  },
+  {
+    "revision": "773597c586f3c7db1609",
+    "url": "/kubash/build/static/js/runtime~main.8c8efe55.js"
+  }
+]);
