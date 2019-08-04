c644e9b8f101dd3dc7ebde65cc8af5a860f2b587
Peter Granderson <solace@fastmail.fm>
2019-08-04 14:15:19 -0500

    add build

---
+++ build/precache-manifest.edb507b10f7c61138f4894b020d786cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75f7d721c758ed1da96247098c41cf0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "751af873e4ae2d8ccf15",
+    "url": "/static/css/main.466b9ff9.chunk.css"
+  },
+  {
+    "revision": "2a8343317e2997c7f548",
+    "url": "/static/js/2.fbbc3172.chunk.js"
+  },
+  {
+    "revision": "751af873e4ae2d8ccf15",
+    "url": "/static/js/main.c20d155f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
