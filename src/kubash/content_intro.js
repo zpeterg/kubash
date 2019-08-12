e90003e3eed4ec69818cfafd0df9659e6d41c819
solace@fastmail.fm <ubuntu@keepcalm.localdomain>
2019-08-11 21:35:10 -0500

    fill in basic bash

--- src/kubash/content_intro.js
+++ src/kubash/content_intro.js
@@ -44,13 +44,74 @@ export default {
     {
       say: [
         'And here you go! Look right below and you\'ll see that you have a file. The file is "myfile.txt".',
-        'The part after the period ("txt") is called the File Extension. It helps you know that kind of file it is. This file is a text file. That means it\'s for a person to read.',
-        'There are other kinds of files - like ".js", which is a JavaScript file for computers to read.',
         'Type "c" to continue.',
       ],
       output: ['myfile.txt'],
       expect: ['c'],
       other: ['Oops! Just type "c" to continue.'],
+    },
+    {
+      say: [
+        'You might be wondering: But what IS a file?',
+        'You might be wondering: But what IS a file? And what is myfile.txt?',
+        'Well, let\'s check out myfile.txt. A simple way to see what\'s in a file is to concatentate "cat" a file.',
+        'Type "cat myfile.txt',
+      ],
+      expect: ['cat myfile.txt'],
+      other: [
+        'Ya, no. Try typing just "cat myfile.txt", except (of course) without the quotes.',
+      ],
+    },
+    {
+      say: [
+        'And there you go! Look below and read what is inside of the file "myfile.txt". Remember it\'s in white because it\'s what you actuall see in bash.',
+        'You can see that "myfile.txt" is the beginning of a story. It is "text" - writing meant for a person to read.',
+        'You can even tell that it\'s text because it says ".txt" at the end.',
+        'Some files end with other things. Like ".js" means that it\'s a JavaScript file. A JavaScript file is a set of instructions for a computer to read.',
+        'Type "c" or "continue".',
+      ],
+      output: [
+        'Once upon a time, there was a princess and a brave knight.',
+        '',
+      ],
+      expect: ['c', 'continue'],
+      other: ['Hmmm, just type "continue".'],
+    },
+    {
+      say: [
+        "You're probably wondering how you can add to that story. But hold on, let's first get you a little more used to handling files.",
+        "In fact, let's start with making a new one.",
+        'It\'s easy to make a file. Just type "touch fishfile.txt".',
+      ],
+      expect: ['touch fishfile.txt'],
+      other: ['Try again. Type "touch fishfile.txt".'],
+    },
+    {
+      say: [
+        "Great! Now, you may be thinking: But nothing happened! But you'd be wrong. The file was made, but you just haven't told bash to show it to you.",
+        'So let\'s ask bash to show it to us. Type "ls" like before to "list" your files.',
+      ],
+      expect: ['ls'],
+      other: ['Ya, no, nothing else. Just type "ls" without the quotes.'],
+    },
+    {
+      say: [
+        "Good! Now look below and you'll see your file, along with the one that was already there.",
+        "There's actually another way to see files that looks a little nicer sometimes - in nice little columns like an index in a book.",
+        "Let's try that. Just type ls -l.",
+      ],
+      output: ['fishfile.txt'],
+      expect: ['ls -l'],
+      other: ['Ya, no, nothing else. Just type "ls" without the quotes.'],
+    },
+    {
+      say: [
+        "And there you go! Look below and you'll see your two files in a list.",
+        'I think we\'re done here. Type "c" to continue.',
+      ],
+      output: ['fishfile.txt', 'myfile.txt'],
+      expect: ['x'],
+      other: ['Just "c" please!'],
     },
     {
       say: [
